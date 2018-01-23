8c32f22e1b870f7fb32f6afb43e83f47e4a9f95d
Roman Eisner <roman.eisner@edmonton.ca>
2018-01-23 11:03:36 -0700

    Use no minimum by default.

--- keywords.py
+++ keywords.py
@@ -1,13 +1,13 @@
 import RAKE #pip install python-rake
 
-def get_keywords(text, score_minimum = 2.0):
+def get_keywords(text, score_minimum = None):
     Rake = RAKE.Rake(RAKE.NLTKStopList())
     keywords = Rake.run(text)
 
     results = []
     for row in keywords:
         score = row[1]
-        if score >= score_minimum:
+        if (score_minimum is None) or (score >= score_minimum):
             results.append({ 'text': row[0], 'score': score })
 
     return(results)
