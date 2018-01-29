b283900971af70c890a628bd6309317510f97bd7
Roman Eisner <roman.eisner@edmonton.ca>
2018-01-29 14:27:12 -0700

    Added textrank.

--- keywords.py
+++ keywords.py
@@ -1,6 +1,16 @@
 import RAKE #pip install python-rake
+import textrank #pip install git+git://github.com/davidadamojr/TextRank.git
 
-def get_keywords(text, score_minimum = None):
+def get_keywords(text, library = "RAKE"):
+    if library == 'RAKE':
+        return(get_rake_keywords(text))
+    elif library == 'textrank':
+        return(get_textrank_keywords(text))
+    else:
+        return({'keywords error': 'Invalid keywords library'})
+
+def get_rake_keywords(text, score_minimum = None):
+    results = []
     Rake = RAKE.Rake(RAKE.NLTKStopList())
     keywords = Rake.run(text)
 
@@ -9,5 +19,14 @@ def get_keywords(text, score_minimum = None):
         score = row[1]
         if (score_minimum is None) or (score >= score_minimum):
             results.append({ 'text': row[0], 'score': score })
+
+    return(results)
+
+def get_textrank_keywords(text):
+    results = []
+    keywords = textrank.extract_key_phrases(text)
+
+    for word in keywords:
+        results.append({ 'text': word })
 
     return(results)
